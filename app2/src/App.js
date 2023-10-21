54bb243c113aff47c03abf312f32a112d906bc62
ProgramadorGod <pipenet12@gmail.com>
2023-10-21 16:10:34 -0500

    Manually

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,46 +1,67 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import "./App.css";
 
 export default function App() {
+  const [homeColor, setHomeColor] = useState('blue');
+  const [aboutColor, setAboutColor] = useState('blue');
+  const [contactColor, setContactColor] = useState('blue');
+
+  const changeColor = (currentColor) => {
+    return currentColor === 'blue' ? 'red' : 'blue';
+  };
+
+  const handleHomeClick = () => {
+    setHomeColor((prevColor) => changeColor(prevColor));
+    setAboutColor(("blue"));
+    setContactColor(("blue"));
+  };
+
+  const handleAboutClick = () => {
+    setAboutColor((prevColor) => changeColor(prevColor));
+    setHomeColor(("blue"));
+    setContactColor(("blue"));
+
+  };
+
+  const handleContactClick = () => {
+    setHomeColor(("blue"));
+    setAboutColor(("blue"));
+    setContactColor((prevColor) => changeColor(prevColor));
+  };
+
   return (
     <>
-
-
     <Router>
       <div className="linksss">
-      <Link to='/' className="Home">Home</Link>
-      <Link to='/About'className="About">About</Link>
-      <Link to='/Contact' className="Contact">Contact</Link>
+        <Link to='/' className={`Home ${homeColor}`} onClick={handleHomeClick}>Home</Link>
+        <Link to='/About' className={`About ${aboutColor}`} onClick={handleAboutClick}>About</Link>
+        <Link to='/Contact' className={`Contact ${contactColor}`} onClick={handleContactClick}>Contact</Link>
       </div>
 
       <Routes>
-      <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
 
       </Routes>
-      
     </Router>
+
     
     </>
+
   );
 }
 
 function Home() {
-  return "AA";
+  return "Home";
 }
 
 function About() {
-  return <h2>About</h2>;
+  return "About";
 }
 
 function Contact() {
-  return <h2>Contact</h2>;
+  return "Contact";
 }
-
