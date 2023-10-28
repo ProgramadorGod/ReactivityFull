9ee81a7ecb1494e3966b41f2bea364de455e982b
ProgramadorGod <pipenet12@gmail.com>
2023-10-28 11:42:58 -0500

    Upgrated a lot of things

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,46 +1,56 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link, Outlet, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import "./App.css";
 
-const links = [
-  { to: '/', label: 'Home' },
-  { to: '/About', label: 'About' },
-  { to: '/Contact', label: 'Contact' }
-];
-
 export default function App() {
-  const [activeLink, setActiveLink] = useState(null);
+  const [homeColor, setHomeColor] = useState('blue');
+  const [aboutColor, setAboutColor] = useState('blue');
+  const [contactColor, setContactColor] = useState('blue');
 
-  const changeColor = (currentColor) => (currentColor === 'blue' ? 'red' : 'blue');
+  const changeColor = (currentColor) => {
+    return currentColor === 'blue' ? 'red' : 'blue';
+  };
 
-  const handleLinkClick = (index) => {
-    setActiveLink(index);
+  const handleHomeClick = () => {
+    setHomeColor((prevColor) => changeColor(prevColor));
+    setAboutColor(("blue"));
+    setContactColor(("blue"));
+  };
+
+  const handleAboutClick = () => {
+    setAboutColor((prevColor) => changeColor(prevColor));
+    setHomeColor(("blue"));
+    setContactColor(("blue"));
+
+  };
+
+  const handleContactClick = () => {
+    setHomeColor(("blue"));
+    setAboutColor(("blue"));
+    setContactColor((prevColor) => changeColor(prevColor));
   };
 
   return (
     <>
-      <Router>
-        <div className="linksss">
-          {links.map((link, index) => (
-            <NavLink
-              key={index}
-              to={link.to}
-              className={`${link.label} ${activeLink === index ? changeColor('blue') : 'blue'}`}
-              onClick={() => handleLinkClick(index)}
-            >
-              {link.label}
-            </NavLink>
-          ))}
-        </div>
+    <Router>
+      <div className="linksss">
+        <Link to='/' className={`Home ${homeColor}`} onClick={handleHomeClick}>Home</Link>
+        <Link to='/About' className={`About ${aboutColor}`} onClick={handleAboutClick}>About</Link>
+        <Link to='/Contact' className={`Contact ${contactColor}`} onClick={handleContactClick}>Contact</Link>
+      </div>
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Contact' element={<Contact />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
+
+      </Routes>
+    </Router>
+
+    
     </>
+
   );
 }
 
